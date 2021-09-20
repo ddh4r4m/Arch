14ab5e53e6fe6b18ffcaba2194c8748e858e6024
rkstrdee <rkstrdee@gmail.com>
2021-09-21 01:36:39 +0530

    installation script updated

--- install.sh
+++ install.sh
@@ -9,12 +9,14 @@ if [ "$UID" -eq "$ROOT_UID" ]; then
   PLASMA_DIR="/usr/share/plasma/desktoptheme"
   LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
   KVANTUM_DIR="/usr/share/Kvantum"
+  KONSOLE_DIR="/usr/share/konsole"
   WALLPAPER_DIR="/usr/share/wallpapers"
 else
   SCHEMES_DIR="$HOME/.local/share/color-schemes"
   PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
   LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
   KVANTUM_DIR="$HOME/.config/Kvantum"
+  KONSOLE_DIR="$HOME/.local/share/konsole"
   WALLPAPER_DIR="$HOME/.local/share/wallpapers"
 fi
 
@@ -25,18 +27,21 @@ THEME_NAME=Arch
 [[ ! -d ${LOOKFEEL_DIR} ]] && mkdir -p ${LOOKFEEL_DIR}
 [[ ! -d ${KVANTUM_DIR} ]] && mkdir -p ${KVANTUM_DIR}
 [[ ! -d ${WALLPAPER_DIR} ]] && mkdir -p ${WALLPAPER_DIR}
+[[ ! -d ${KONSOLE_DIR} ]] && mkdir -p ${KONSOLE_DIR}
 
 install() {
   local name=${1}
 
   [[ -d ${PLASMA_DIR}/${name} ]] && rm -rf ${PLASMA_DIR}/${name}*
   [[ -f ${SCHEMES_DIR}/${name}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}*.colors
-  [[ -d ${LOOKFEEL_DIR}/com.github.vinceliuice.${name} ]] && rm -rf ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}*
+  [[ -d ${LOOKFEEL_DIR}/${name} ]] && rm -rf ${LOOKFEEL_DIR}/${name}*
   [[ -d ${KVANTUM_DIR}/${name} ]] && rm -rf ${KVANTUM_DIR}/${name}*
+  [[ -d ${KONSOLE_DIR}/${name} ]] && rm -rf ${KONSOLE_DIR}/${name}*
   [[ -d ${WALLPAPER_DIR}/${name} ]] && rm -rf ${WALLPAPER_DIR}/${name}
 
   cp -r ${SRC_DIR}/color-schemes/*.colors                                            ${SCHEMES_DIR}
   cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
+  cp -r ${SRC_DIR}/konsole/*                                                         ${KONSOLE_DIR}
   cp -r ${SRC_DIR}/plasma/desktoptheme/*                                             ${PLASMA_DIR}
   cp -r ${SRC_DIR}/color-schemes/${name}.colors                                      ${PLASMA_DIR}/${name}/colors
   cp -r ${SRC_DIR}/color-schemes/${name}Dark.colors                                  ${PLASMA_DIR}/${name}-dark/colors
